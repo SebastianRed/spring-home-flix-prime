618de416da2157a580ed520bb38cb1a4af274364
Sebastián Rojo <sebastianrojocollao@gmail.com>
2022-03-05 10:35:28 -0300

    Inicializa repositorio

---
+++ src/main/java/cl/sebastianrojo/springhomeflixprime/SpringHomeFlixPrimeApplication.java
@@ -0,0 +1,13 @@
+package cl.sebastianrojo.springhomeflixprime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomeFlixPrimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHomeFlixPrimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/sebastianrojo/springhomeflixprime/SpringHomeFlixPrimeApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sebastianrojo.springhomeflixprime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHomeFlixPrimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
